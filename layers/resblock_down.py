6906f64198b3fa5047e3ed11147bd5ea7a6451c5
Thalles Silva <thalles753@gmail.com>
2019-08-08 11:05:27 -0300

    fix identation

--- layers/resblock_down.py
+++ layers/resblock_down.py
@@ -13,7 +13,8 @@ class BlockDown(tf.keras.Model):
         self.learnable_sc = (in_channels != out_channels) or downsample
         hidden_channels = in_channels if hidden_channels is None else hidden_channels
 
-        self.c1 = SNConv2D(hidden_channels, kernel_size=ksize, padding=pad, kernel_initializer=initializer, use_bias=False)
+        self.c1 = SNConv2D(hidden_channels, kernel_size=ksize, padding=pad, kernel_initializer=initializer,
+                           use_bias=False)
 
         self.b1 = tf.keras.layers.BatchNormalization()
         self.b2 = tf.keras.layers.BatchNormalization()
@@ -22,7 +23,8 @@ class BlockDown(tf.keras.Model):
         self.avg_pool = tf.keras.layers.AveragePooling2D(pool_size=2, strides=2, padding="SAME")
 
         if self.learnable_sc:
-            self.c_sc = SNConv2D(out_channels, kernel_size=1, padding="VALID", kernel_initializer=initializer, use_bias=False)
+            self.c_sc = SNConv2D(out_channels, kernel_size=1, padding="VALID", kernel_initializer=initializer,
+                                 use_bias=False)
 
     def residual(self, x, sn_update, **kwargs):
         h = x
